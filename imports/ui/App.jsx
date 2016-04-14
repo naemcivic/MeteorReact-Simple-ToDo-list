a6250ecd9fd9ec93b756a7dad543b913ca22d176
naemcivic <naemcivic@gmail.com>
2016-04-13 22:02:39 -0400

    add Mongo DB to application, add event handler to App component to insert into database

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,29 +1,42 @@
-import React, { Component } from 'react';
-
+import React, { Component, PropTypes } from 'react';
+import ReactDom from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data'
+import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 // App component - representation of the whole application
-export default class App extends Component {
-  getTasks() {
-    return [
-      {_id: 1, text: 'This is task 1'},
-      {_id: 2, text: 'This is task 2'},
-      {_id: 3, text: 'This is task 3'},
-    ];
-  }
+class App extends Component {
+  handleSubmit (e) {
+    e.preventDefault();
 
+    //cache the text field via React ref
+    text = ReactDom.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), //current time
+    });
+    // clear form
+    ReactDom.findDOMNode(this.refs.textInput).value='';
+  }
   renderTasks () {
-    return this.getTasks().map((task) => (
+    return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
-
   render () {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
-        </header>
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type='text'
+              ref='textInput'
+              placeholder='Enter a task here'
+              />
+          </form>
+    </header>
         <ul>
           {this.renderTasks()}
         </ul>
@@ -31,3 +44,13 @@ export default class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  tasks: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: -1 }} ).fetch(),
+  }
+}, App);
