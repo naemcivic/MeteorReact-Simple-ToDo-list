94dd592768885b7f70ca3d1d42f04b8a0bece429
naemcivic <naemcivic@gmail.com>
2016-04-15 15:07:17 -0400

    correct typo in tasks.js

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -8,6 +8,7 @@ Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
 
+    // Make sure the user is logged in before inserting a task
     if (! Meteor.userId()) {
       throw new Meteor.Error('not-authorized');
     }
@@ -19,15 +20,15 @@ Meteor.methods({
       username: Meteor.user().username,
     });
   },
-
   'tasks.remove'(taskId) {
     check(taskId, String);
+
     Tasks.remove(taskId);
   },
   'tasks.setChecked'(taskId, setChecked) {
-      check(taskId, String);
-      check(setChecked, Boolean);
+    check(taskId, String);
+    check(setChecked, Boolean);
 
-      Tasks.update(taskId, { $set: { checked: setChecked } });
-    },
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
 });
