8ce779ea30982362f477670f41ef03252edf3184
naemcivic <naemcivic@gmail.com>
2016-04-15 23:32:43 -0400

    remove autopublish from meteor, add publish for collection tasks and subscribe in App container

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,6 +4,11 @@ import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('tasks', tasksPublication => () Tasks.find(););
+}
+
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
